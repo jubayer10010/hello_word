9e54352a1b6b08ecf6516d501352ac7f5306f960
admin_pro <jubayermahmud352@gmai.com>
2026-01-21 18:11:33 +0000

    flot got added

--- project/python.py
+++ project/python.py
@@ -3,7 +3,7 @@ name=input("whats your name")
 print (f"hello {name}")
 operator=input("choose an oparator {+-*/}")
 if pterator=="+":
-   fast=input("what's the first number?)
-   sec=input("what's the decond number?)
+   fast=flot(input("what's the first number?))
+   sec=flot(input("what's the decond number?))
    ans=fast+sec
    print (f"the ans is {ans}") 
