d69ecb08a18942c4a0a6a819259107ef9c2d3d8f
admin_pro <jubayermahmud352@gmai.com>
2026-01-21 18:08:44 +0000

    new python file adds

---
+++ project/python.py
@@ -0,0 +1,9 @@
+print ("hello world")
+name=input("whats your name")
+print (f"hello {name}")
+operator=input("choose an oparator {+-*/}")
+if pterator=="+":
+   fast=input("what's the first number?)
+   sec=input("what's the decond number?)
+   ans=fast+sec
+   print (f"the ans is {ans}") 
